6a4cbd01f73656c22eaf70b5d16bd84d441fc7b4
e1y4r <e1y4r@outlook.com>
2020-08-07 17:45:26 +0800

    move to correct folder

---
+++ src/me/elyar/resp/type/RespType.java
@@ -0,0 +1,9 @@
+package me.elyar.resp.type;
+
+/**
+ * RESP数据类型
+ *
+ * @author e1y4r
+ */
+public abstract class RespType {
+}
--- type/RespType.java
+++
@@ -1,9 +0,0 @@
-package me.elyar.resp.type;
-
-/**
- * RESP数据类型
- *
- * @author e1y4r
- */
-public abstract class RespType {
-}
